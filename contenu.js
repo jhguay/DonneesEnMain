262b45628783a7eba70b97b381f35b7d2206ce44
JH Guay <41539595+jhguay@users.noreply.github.com>
2025-09-28 14:14:47 -0400

    Update contenu.js

--- contenu.js
+++ contenu.js
@@ -1,7 +1,7 @@
 // contenus.js
 
 const texte1 = `
-#Nos données très simples sont dans un nuage.
+#Les données sont téléchargées, puis lues. On crée un dataframe appelé df
 import pandas as pd
 source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
 df = pd.read_csv(source, encoding='latin1')
@@ -9,17 +9,20 @@ df
 `;
 
 const texte2 = `
+#Les données sont téléchargées, puis lues. On crée un dataframe appelé df
 import pandas as pd
 source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
 df = pd.read_csv(source, encoding='latin1')
+#On corrige le nom des villes pour qu'elles commencent par une majuscule
 df['ville'] = df['ville'].str.title()
-
+#On fait un dictionnaire qui associe chaque ville à l'une des deux grandes régions.
 regroupement={'Magog':'Reste du Québec',
               'Carignan':'Reste du Québec',
               'Sherbrooke':'Reste du Québec',
               'Montréal':'Métropole',
               'Laval':'Métropole',
 }
+#Avec la méthode map de la variable on utilise le dictionnaire. "Autres villes"  pour les autres.
 df['région'] = df['ville'].map(regroupement).fillna("Autres villes")
 print(df)
 `;
@@ -40,6 +43,7 @@ function formatTexteAvecBr(texte) {
     })
     .join('');
 }
+
 
 
 
