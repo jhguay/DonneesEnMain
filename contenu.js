20c5ab58ebc4c1f42aade2dff0dea9bb50d85b21
JH Guay <41539595+jhguay@users.noreply.github.com>
2025-09-28 18:26:03 -0400

    Update contenu.js

--- contenu.js
+++ contenu.js
@@ -27,6 +27,26 @@ df['région'] = df['ville'].map(regroupement).fillna("Autres villes")
 print(df)
 `;
 
+
+const Z3 = `
+#3. Les données sont téléchargées, puis lues. On crée un dataframe appelé df
+import pandas as pd
+source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
+df = pd.read_csv(source, encoding='latin1')
+#On corrige le nom des villes pour qu'elles commencent par une majuscule
+df['ville'] = df['ville'].str.title()
+#On fait un dictionnaire qui associe chaque ville à l'une des deux grandes régions.
+regroupement={'Magog':'Reste du Québec',
+              'Carignan':'Reste du Québec',
+              'Sherbrooke':'Reste du Québec',
+              'Montréal':'Métropole',
+              'Laval':'Métropole',
+}
+#Avec la méthode map de la variable on utilise le dictionnaire. "Autres villes"  pour les autres.
+df['région'] = df['ville'].map(regroupement).fillna("Autres villes")
+print(df)
+`;
+
 function formatTexteAvecBr(texte) {
   return texte
     .trim()
@@ -43,6 +63,7 @@ function formatTexteAvecBr(texte) {
     })
     .join('');
 }
+
 
 
 
