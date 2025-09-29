bd8cb71c79031578152df8a9fba47a03c091fddf
JH Guay <41539595+jhguay@users.noreply.github.com>
2025-09-29 11:10:17 -0400

    Update contenu.js

--- contenu.js
+++ contenu.js
@@ -9,9 +9,9 @@ df
 `;
 
 const Z2 = `
-%import pandas as pd
-%source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
-$df = pd.read_csv(source, encoding='latin1')
+&import pandas as pd
+&source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
+&df = pd.read_csv(source, encoding='latin1')
 #On corrige le nom des villes pour qu'elles commencent par une majuscule en utilisant la méthode .title()
 df['ville'] = df['ville'].str.title()
 #Toutes les villes devraient avoir une majuscule
@@ -19,10 +19,10 @@ df
 `;
 
 const Z3 = `
-%import pandas as pd
-%source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
-%df = pd.read_csv(source, encoding='latin1')
-%df['ville'] = df['ville'].str.title()
+&import pandas as pd
+&source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
+&df = pd.read_csv(source, encoding='latin1')
+&df['ville'] = df['ville'].str.title()
 #Pour avoir les fréquences d'une variable qualitative, on utilise la méthode .value_counts() 
 t1=df['genre'].value_counts()
 print(t1)
@@ -34,12 +34,10 @@ t3=round(df['genre'].value_counts(normalize=True)*100)
 print(t3)
 `;
 const Z4 = `
-%import pandas as pd
-%source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
-%df = pd.read_csv(source, encoding='latin1')
-#On corrige le nom des villes pour qu'elles commencent par une majuscule en utilisant la méthode .title()
-%df['ville'] = df['ville'].str.title()
-#Toutes les villes devraient avoir une majuscule
+&import pandas as pd
+&source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
+&df = pd.read_csv(source, encoding='latin1')
+&df['ville'] = df['ville'].str.title()
 #On utilise les méthodes mean() et median() pour avoir les informations. C'est seulement valide pour les variables quantitatives
 moyenne=df['age'].mean()
 print(moyenne)
@@ -47,12 +45,10 @@ mediane=df['age'].median()
 print(mediane)
 `;
 const Z5 = `
-#Les données sont téléchargées, puis lues. On crée un dataframe appelé df
-import pandas as pd
-source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
-df = pd.read_csv(source, encoding='latin1')
-#On corrige le nom des villes pour qu'elles commencent par une majuscule en utilisant la méthode .title()
-df['ville'] = df['ville'].str.title()
+&import pandas as pd
+&source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
+&df = pd.read_csv(source, encoding='latin1')
+&df['ville'] = df['ville'].str.title()
 #Pour croiser deux variables qualitatives: les  nombres de répondants
 t4=pd.crosstab(df["ville"], df["joue_musique"],margins=True,margins_name='Total')
 print(t4)
@@ -65,42 +61,37 @@ print(t6)
 `;
 
 const Z6 = `
-#Les données sont téléchargées, puis lues. On crée un dataframe appelé df
-%import pandas as pd
-%source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
-%df = pd.read_csv(source, encoding='latin1')
-#On corrige le nom des villes pour qu'elles commencent par une majuscule en utilisant la méthode .title()
-%df['ville'] = df['ville'].str.title()
+&import pandas as pd
+&source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
+&df = pd.read_csv(source, encoding='latin1')
+&df['ville'] = df['ville'].str.title()
 #Pour croiser une variable qualitative et une variable quantitative.
 t7 = df.groupby("genre")["age"].mean().reset_index()
 print(t7)
 `;
 
 const Z7 = `
-#Les données sont téléchargées, puis lues. On crée un dataframe appelé df
-%import pandas as pd
-%source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
-%df = pd.read_csv(source, encoding='latin1')
-#On corrige le nom des villes pour qu'elles commencent par une majuscule en utilisant la méthode .title()
-%df['ville'] = df['ville'].str.title()
-#Pour croiser deux variables quantitatives
+&import pandas as pd
+&source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
+&df = pd.read_csv(source, encoding='latin1')
+&df['ville'] = df['ville'].str.title()
+#Pour croiser deux variables quantitatives, on utilise la méthode corr() avec comme paramètre la seconde variable quantitative.
 correlation = df["age"].corr(df["nb_ordi"])
 print(correlation)
 `;
 
 const Z8 = `
-#Les données sont téléchargées, puis lues. On crée un dataframe appelé df
-%import pandas as pd
-%source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
-%df = pd.read_csv(source, encoding='latin1')
-#On corrige le nom des villes pour qu'elles commencent par une majuscule en utilisant la méthode .title()
-%df['ville'] = df['ville'].str.title()
+&import pandas as pd
+&source="https://raw.githubusercontent.com/jhguay/DonneesEnMain/main/donneesFamille.csv"
+&df = pd.read_csv(source, encoding='latin1')
+&df['ville'] = df['ville'].str.title()
 #On importe une autre module d'outils, le matplotlib
 import matplotlib.pyplot as plt
 #On travaille avec la variable qualitative, genre, on utilisera deux couleurs (stéréotypées!)
 couleurs=['lightcoral','skyblue']
+
 #Pour illustrer une variable qualitative
-#On obtient le tableau t3
+#On obtient à nouveau le tableau t3
 t3=round(df['genre'].value_counts(normalize=True)*100)
 #On utilise pour ce tableau la méthode plot pour dessiner un graphique et on ajoute des précisions
 t3.plot(kind="pie", colors=couleurs)
@@ -108,6 +99,7 @@ plt.title("Réparition des deux genres")
 plt.xlabel("Âge")
 plt.ylabel("Nombre de personnes")
 plt.show()
+
 #Pour illustrer une variable quantitative
 # Histogramme, très utile pour départager une distribution numérique et la représenter graphiquement.
 plt.hist(df["age"], bins=3, edgecolor="black")  # bins = nombre de classes
@@ -116,6 +108,14 @@ plt.xlabel("Âge")
 plt.ylabel("Nombre de personnes")
 plt.show()
 
+#Pour illustrer une variable quantitative et une variable qualitative
+t7 = df.groupby("genre")["age"].mean().reset_index()
+t7.plot(kind="bar",color=couleurs) 
+plt.title("Âge moyen selon le genre")
+plt.xlabel("Genres") 
+plt.ylabel("Âges") 
+plt.grid(axis='y', linestyle='--') plt.xticks(rotation=0) 
+plt.show()
 `;
 
 const Source = `
@@ -176,6 +176,7 @@ function formatTexteAvecBr(texte) {
     })
     .join('');
 }
+
 
 
 
